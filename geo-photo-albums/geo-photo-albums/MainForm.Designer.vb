145ea738dfe4e55e90520efbe60ad8f1ecd69928
shawarmakarma@gmail.com <shawarmakarma@gmail.com@c538408c-5b67-af6f-44c4-f767c21ebf1d>
2012-05-30 08:14:23 +0000

    Get started on first tab for CSV sorting

--- geo-photo-albums/geo-photo-albums/MainForm.Designer.vb
+++ geo-photo-albums/geo-photo-albums/MainForm.Designer.vb
@@ -22,29 +22,160 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MainTab = New System.Windows.Forms.TabControl()
+        Me.SortCsvTab = New System.Windows.Forms.TabPage()
+        Me.btnDestFile = New System.Windows.Forms.Button()
+        Me.btnSourceDir = New System.Windows.Forms.Button()
+        Me.lblDestination = New System.Windows.Forms.Label()
+        Me.txtDestination = New System.Windows.Forms.TextBox()
+        Me.btnDestDir = New System.Windows.Forms.Button()
+        Me.btnSourceFile = New System.Windows.Forms.Button()
+        Me.lblSource = New System.Windows.Forms.Label()
+        Me.txtSource = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.MainTab.SuspendLayout()
+        Me.SortCsvTab.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'MainTab
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.MainTab.Controls.Add(Me.SortCsvTab)
+        Me.MainTab.Controls.Add(Me.TabPage2)
+        Me.MainTab.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MainTab.Location = New System.Drawing.Point(0, 0)
+        Me.MainTab.Name = "MainTab"
+        Me.MainTab.SelectedIndex = 0
+        Me.MainTab.Size = New System.Drawing.Size(562, 373)
+        Me.MainTab.TabIndex = 0
+        '
+        'SortCsvTab
+        '
+        Me.SortCsvTab.Controls.Add(Me.btnDestFile)
+        Me.SortCsvTab.Controls.Add(Me.btnSourceDir)
+        Me.SortCsvTab.Controls.Add(Me.lblDestination)
+        Me.SortCsvTab.Controls.Add(Me.txtDestination)
+        Me.SortCsvTab.Controls.Add(Me.btnDestDir)
+        Me.SortCsvTab.Controls.Add(Me.btnSourceFile)
+        Me.SortCsvTab.Controls.Add(Me.lblSource)
+        Me.SortCsvTab.Controls.Add(Me.txtSource)
+        Me.SortCsvTab.Location = New System.Drawing.Point(4, 22)
+        Me.SortCsvTab.Name = "SortCsvTab"
+        Me.SortCsvTab.Padding = New System.Windows.Forms.Padding(3)
+        Me.SortCsvTab.Size = New System.Drawing.Size(554, 347)
+        Me.SortCsvTab.TabIndex = 0
+        Me.SortCsvTab.Text = "Sort CSV"
+        Me.SortCsvTab.UseVisualStyleBackColor = True
+        '
+        'btnDestFile
+        '
+        Me.btnDestFile.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDestFile.Location = New System.Drawing.Point(430, 35)
+        Me.btnDestFile.Name = "btnDestFile"
+        Me.btnDestFile.Size = New System.Drawing.Size(55, 23)
+        Me.btnDestFile.TabIndex = 7
+        Me.btnDestFile.Text = "File..."
+        Me.btnDestFile.UseVisualStyleBackColor = True
+        '
+        'btnSourceDir
+        '
+        Me.btnSourceDir.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSourceDir.Location = New System.Drawing.Point(491, 6)
+        Me.btnSourceDir.Name = "btnSourceDir"
+        Me.btnSourceDir.Size = New System.Drawing.Size(55, 23)
+        Me.btnSourceDir.TabIndex = 6
+        Me.btnSourceDir.Text = "Folder..."
+        Me.btnSourceDir.UseVisualStyleBackColor = True
+        '
+        'lblDestination
+        '
+        Me.lblDestination.AutoSize = True
+        Me.lblDestination.Location = New System.Drawing.Point(8, 40)
+        Me.lblDestination.Name = "lblDestination"
+        Me.lblDestination.Size = New System.Drawing.Size(60, 13)
+        Me.lblDestination.TabIndex = 5
+        Me.lblDestination.Text = "Destination"
+        '
+        'txtDestination
+        '
+        Me.txtDestination.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtDestination.Location = New System.Drawing.Point(85, 37)
+        Me.txtDestination.Name = "txtDestination"
+        Me.txtDestination.Size = New System.Drawing.Size(339, 20)
+        Me.txtDestination.TabIndex = 4
+        '
+        'btnDestDir
+        '
+        Me.btnDestDir.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDestDir.Location = New System.Drawing.Point(491, 35)
+        Me.btnDestDir.Name = "btnDestDir"
+        Me.btnDestDir.Size = New System.Drawing.Size(55, 23)
+        Me.btnDestDir.TabIndex = 3
+        Me.btnDestDir.Text = "Folder..."
+        Me.btnDestDir.UseVisualStyleBackColor = True
+        '
+        'btnSourceFile
+        '
+        Me.btnSourceFile.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSourceFile.Location = New System.Drawing.Point(430, 6)
+        Me.btnSourceFile.Name = "btnSourceFile"
+        Me.btnSourceFile.Size = New System.Drawing.Size(55, 23)
+        Me.btnSourceFile.TabIndex = 2
+        Me.btnSourceFile.Text = "File..."
+        Me.btnSourceFile.UseVisualStyleBackColor = True
+        '
+        'lblSource
+        '
+        Me.lblSource.AutoSize = True
+        Me.lblSource.Location = New System.Drawing.Point(8, 11)
+        Me.lblSource.Name = "lblSource"
+        Me.lblSource.Size = New System.Drawing.Size(41, 13)
+        Me.lblSource.TabIndex = 1
+        Me.lblSource.Text = "Source"
+        '
+        'txtSource
+        '
+        Me.txtSource.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSource.Location = New System.Drawing.Point(85, 8)
+        Me.txtSource.Name = "txtSource"
+        Me.txtSource.Size = New System.Drawing.Size(339, 20)
+        Me.txtSource.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(554, 347)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(562, 373)
+        Me.Controls.Add(Me.MainTab)
         Me.Name = "MainForm"
-        Me.Text = "Form1"
+        Me.Text = "GeoPhotoAlbums"
+        Me.MainTab.ResumeLayout(False)
+        Me.SortCsvTab.ResumeLayout(False)
+        Me.SortCsvTab.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents MainTab As System.Windows.Forms.TabControl
+    Friend WithEvents SortCsvTab As System.Windows.Forms.TabPage
+    Friend WithEvents lblDestination As System.Windows.Forms.Label
+    Friend WithEvents txtDestination As System.Windows.Forms.TextBox
+    Friend WithEvents btnDestDir As System.Windows.Forms.Button
+    Friend WithEvents btnSourceFile As System.Windows.Forms.Button
+    Friend WithEvents lblSource As System.Windows.Forms.Label
+    Friend WithEvents txtSource As System.Windows.Forms.TextBox
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents btnSourceDir As System.Windows.Forms.Button
+    Friend WithEvents btnDestFile As System.Windows.Forms.Button
 
 End Class
--- geo-photo-albums/geo-photo-albums/MainForm.vb
+++ geo-photo-albums/geo-photo-albums/MainForm.vb
@@ -1,4 +1,27 @@
 ﻿Imports System.Runtime.CompilerServices
 
 Class MainForm
+
+    Private Sub btnSourceFile_Click(sender As System.Object, e As System.EventArgs) Handles btnSourceFile.Click
+        Dim ofd As New OpenFileDialog
+        ofd.DefaultExt = "csv"
+        If System.IO.File.Exists(txtSource.Text) Then
+            ofd.FileName = txtSource.Text
+            ofd.InitialDirectory = System.IO.Path.GetDirectoryName(txtSource.Text)
+        ElseIf System.IO.Directory.Exists(txtSource.Text) Then
+            ofd.InitialDirectory = txtSource.Text
+        End If
+        ofd.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+        ofd.Multiselect = True
+        ofd.Title = "Find CSV File(s)..."
+        If ofd.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+            If ofd.FileNames.Count > 1 Then
+                txtSource.Text = String.Join(",", ofd.FileNames.Select(Function(s As String)
+                                                                           Return """" + s + """"
+                                                                       End Function))
+            Else
+                txtSource.Text = ofd.FileName
+            End If
+        End If
+    End Sub
 End Class
